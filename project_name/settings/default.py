aa86e17c7eefb628d1117dfc37223849f715b10a
chrisforrette <chris@chrisforrette.com>
2015-05-12 14:07:08 -0700

    Fixing issue with BASE_DIR

--- project_name/settings/default.py
+++ project_name/settings/default.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 PROJECT_DIR = os.path.join(BASE_DIR, '{{ project_name }}')
 APPS_DIR = os.path.join(PROJECT_DIR, 'apps')
 
